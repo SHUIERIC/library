44ab7bc9378633be6d5e183a024dd77de432d997
SHUIERIC <229310374+SHUIERIC@users.noreply.github.com>
2025-10-14 20:49:32 +1100

    Add card display for books

---
+++ script.js
@@ -0,0 +1,57 @@
+const myLibrary = []
+
+function Book (title, author, pageNumber, read) {
+     if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+    this.id = crypto.randomUUID()
+    this.title = title;
+    this.author = author;
+    this.pageNumber = pageNumber;
+    this.read = read;
+
+    this.sayBook = function () {
+        console.log(this.title)
+    }
+}
+
+
+function addBookToLibrary (title, author, pageNumber, read) {
+    const book = new Book(title, author, pageNumber, read);
+    myLibrary.push(book)
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+addBookToLibrary("1984", "George Orwell", 328, true);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
+
+
+
+function displayBook () { 
+const container = document.querySelector(".bookContainer");
+
+myLibrary.forEach ((book) => {
+    const card = document.createElement("div")
+    card.classList.add("book-card")
+
+    card.innerHTML = `
+        <div></div>
+        <div> ${book.title} </div>
+        <div> Author: ${book.author} </div>
+        <div> Page Number: ${book.pageNumber} </div>
+        <div> Read: ${book.read ?'Yes' : "No"} </div>
+        <div></div>
+    `
+    container.appendChild(card)
+
+}
+)
+}
+
+displayBook ()
+
+
+
+
+
+
