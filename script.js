cb18432db10acc076f76e5c21e81cbcd79bef430
SHUIERIC <229310374+SHUIERIC@users.noreply.github.com>
2025-10-14 21:55:00 +1100

    Add dialog for book adding functionality

--- script.js
+++ script.js
@@ -28,7 +28,9 @@ addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
 
 
 function displayBook () { 
-const container = document.querySelector(".bookContainer");
+    const container = document.querySelector(".bookContainer");
+    container.innerHTML = "";
+
 
 myLibrary.forEach ((book) => {
     const card = document.createElement("div")
@@ -43,13 +45,38 @@ myLibrary.forEach ((book) => {
         <div></div>
     `
     container.appendChild(card)
-
 }
 )
 }
 
 displayBook ()
 
+
+const showBtn = document.querySelector(".addBtn")
+const addBook = document.querySelector(".addBook")
+const confirmBtn = document.querySelector("#confirmBtn")
+
+showBtn.addEventListener("click", ()=> {
+    addBook.showModal()
+})
+
+confirmBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pageNumber = document.getElementById("pageNumber").value
+    const readStatus = document.querySelector("input[name=readStatus]:checked")
+
+
+    addBookToLibrary(title, author, pageNumber, readStatus);
+    displayBook();
+    addBook.close();
+
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pageNumber").value = ""
+
+})
 
 
 
